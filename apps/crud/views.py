582aa6dcb4bb8facfcfaa4aeaa0ab5b3db5c3d96
Kudodxd <tcnminh@gmail.com>
2024-05-23 03:28:30 +0900

    Processing at page 119

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
+
+from apps.app import db
+from apps.crud.forms import UserForm
+from apps.crud.models import User
 
 # Create CRUD app using Blueprint
 crud = Blueprint(
@@ -13,3 +17,34 @@ crud = Blueprint(
 @crud.route("/")
 def index():
     return render_template("crud/index.html")
+
+
+@crud.route("/sql")
+def sql():
+    # db.session.query(User).all()
+    # db.session.query(User).first()
+    # db.session.query(User).get(2)
+    # db.session.query(User).count()
+    db.session.query(User).paginate(page=2, per_page=10)
+    User.query.filter_by(id=2, username="admin").all()
+    return "Please confirm console log"
+
+
+@crud.route("/users/new", methods=["GET", "POST"])
+def create_user():
+    # Instance UserForm
+    form = UserForm()
+    # Validate form's value
+    if form.validate_on_submit():
+        # Create user
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+        # Add user and commit
+        db.session.add(user)
+        db.session.commit()
+        # Redirect to user HP
+        return redirect(url_for("/crud.users"))
+    return render_template("crud/create.html", form=form)
