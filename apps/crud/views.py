723c6a12bf89c5470d8790e23268760e50b8840b
Kudodxd <tcnminh@gmail.com>
2024-05-24 02:18:49 +0900

    Adding edit and delete user's feature

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -46,5 +46,40 @@ def create_user():
         db.session.add(user)
         db.session.commit()
         # Redirect to user HP
-        return redirect(url_for("/crud.users"))
+        return redirect(url_for("crud.users"))
     return render_template("crud/create.html", form=form)
+
+
+@crud.route("/users")
+def users():
+    """User list"""
+    users = User.query.all()
+    return render_template("crud/index.html", users=users)
+
+
+@crud.route("/users/<user_id>", methods=["GET", "POST"])
+def edit_user(user_id):
+    form = UserForm()
+
+    # Filtering user using User model
+    user = User.query.filter_by(id=user_id).first()
+
+    # Submiting form means update user and redirects to Users table
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("crud.users"))
+
+    # if method is GET then return edit.html
+    return render_template("crud/edit.html", user=user, form=form)
+
+
+@crud.route("/users/<user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for("crud.users"))
